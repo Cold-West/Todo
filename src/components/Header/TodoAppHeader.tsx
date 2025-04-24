3f50bbd1c6800a0f3f75275d8939db852b668bb0
Cold-West <grigoriy777kub@gmail.com>
2025-04-24 17:12:32 +0300

    LEARN-004 first

--- src/components/Header/TodoAppHeader.tsx
+++ src/components/Header/TodoAppHeader.tsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 import "./TodoAppHeader.css";
 import DatePicker from "react-datepicker";
 type TodoHeaderProps = {
-  create: (text: string, date: Date | null) => void;
+  create: (title: string, text: string, date: Date | null) => void;
   check: () => void;
 };
 
 export const TodoAppHeader = (props: TodoHeaderProps) => {
   const { create, check } = props;
 
+  const [todoTitle, setTodoTitle] = useState("");
   const [todoText, setTodoText] = useState("");
   const [startDate, setStartDate] = useState<Date | null>(new Date());
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (todoText !== "") {
-      create(todoText, startDate);
+    if (todoTitle !== "") {
+      create(todoTitle, todoText, startDate);
+      setTodoTitle("");
       setTodoText("");
       setStartDate(new Date());
     } else alert("поле не может быть пустым");
@@ -25,18 +27,29 @@ export const TodoAppHeader = (props: TodoHeaderProps) => {
       <button type="button" className="TodoAppCheckAll" onClick={check}>
         ✓
       </button>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Что надо сделать?"
-          className="TodoAppInput"
-          value={todoText}
-          onChange={(e) => setTodoText(e.target.value)}
-        />
-      </form>
+      <div className="headerInputs">
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="Что надо сделать?"
+            className="TodoAppInput"
+            value={todoTitle}
+            onChange={(e) => setTodoTitle(e.target.value)}
+          />
+        </form>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            placeholder="Описание"
+            className="TodoAppInput"
+            value={todoText}
+            onChange={(e) => setTodoText(e.target.value)}
+          />
+        </form>
+      </div>
       <DatePicker
         closeOnScroll={(e) => e.target === document}
-        className="datePickerInput"
+        className="headerDatePickerInput"
         selected={startDate}
         onChange={(date) => setStartDate(date)}
       />
